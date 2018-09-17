5c0a32d7dd4449d8858a489b1a6f2bf847070d04
Serhii-Yasenev <serhii_yasenev@epam.com>
2018-09-17 13:16:10 +0300

    upd

--- SuperApp.Backoffice/Properties/AssemblyInfo.cs
+++ SuperApp.Backoffice/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SuperApp.Backoffice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SuperApp.Backoffice")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
